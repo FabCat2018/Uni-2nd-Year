90f4e4ed2bd4a879114271ec06d9cc160153c418
Fabio Cataleta <fcataleta@gmail.com>
2019-03-13 17:28:54 +0000

    Got to multiple scenes section in tutorial. Attempting to implement functionality and basic layout for MovieGUI

--- Backend/MovieGUI.java
+++ Backend/MovieGUI.java
@@ -1,28 +1,24 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
-public class MovieGUI extends Application implements EventHandler<ActionEvent> {
+public class MovieGUI extends Application {
     /**
      * @param database
      * @param ur
      * @param user
      */
 
-    Button loginButton = new Button("Login");
-    Button registerButton = new Button("Register");
-    Button addButton = new Button("Add Review to 'Spectre'");
-    Button editButton = new Button("Edit Review");
-    Button deleteButton = new Button("Delete Review");
-
     MovieManager database = MovieManager.getInstance();
-
     UnregisteredUser ur = new UnregisteredUser();
     RegisteredUser user = null;
+
+    Stage window;
+    Scene registerScene, loginScene, mainScene, movieScene, textScene;
 
     public static void main(String[] args) {
         launch(args);
@@ -32,30 +28,77 @@ public class MovieGUI extends Application implements EventHandler<ActionEvent> {
     @Override
     public void start (Stage primaryStage) throws Exception {
         database.addMovie("Spectre", "Action", 4, 3, 4, 4, 5, true);
-        primaryStage.setTitle("Movie Review System");
+        window = primaryStage;
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(registerButton);
+        /* REGISTER SCENE SECTION */
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+        //Register Scene Functionality
+        Label registerLabel = new Label("Please register your details with us");
+        TextField userNameR = new TextField();
+        TextField passwordR = new TextField();
+        Button registerButton = new Button("Register");
+        registerButton.setOnAction(e -> {
+            user = ur.register(userNameR.getText(), passwordR.getText());
+            window.setScene(loginScene);
+        });
 
-    @Override
-    public void handle (ActionEvent event) {
-        if (event.getSource() == registerButton)
-            user = ur.register(userName, password);
-        if (event.getSource() == loginButton)
-            user.login(userName, password);
-        if (event.getSource() == addButton && user != null) {
-            Movie movie = user.searchMovie("Spectre");
-            System.out.println(movie.toString());
-            Review review = user.writeReview(movie, "This is testing if review has been created");
-        }
-        if (event.getSource() == editButton)
-            user.editReview(movie, review, "This review had its body changed");
-        if (event.getSource() == deleteButton)
-            user.deleteReview(movie, review);
+        //Register Scene Layout
+        VBox registerLayout = new VBox(20);
+        registerLayout.getChildren().addAll(registerLabel, userNameR, passwordR, registerButton);
+        registerScene = new Scene(registerLayout, 300, 200);
+
+        /* END OF REGISTER SCENE SECTION */
+
+
+        /* LOGIN SCENE SECTION */
+
+        //Login Scene Functionality
+        Label loginLabel = new Label("Please enter your login details");
+        TextField userNameL = new TextField();
+        TextField passwordL = new TextField();
+        Button loginButton = new Button("Login");
+        loginButton.setOnAction(e -> {
+            user.login(userNameL.getText(), passwordL.getText());
+            window.setScene(mainScene);
+        });
+
+        //Login Scene Layout
+        VBox loginLayout = new VBox(20);
+        loginLayout.getChildren().addAll(loginLabel, userNameL, passwordL, loginButton);
+        loginScene = new Scene(loginLayout, 300, 200);
+
+        /* END OF LOGIN SCENE SECTION */
+        
+
+        /* MAIN SCENE SECTION */
+
+        //Main Scene Functionality
+
+        //Main Scene Layout
+
+        /* END OF MAIN SCENE SECTION */
+
+
+        /* MOVIE SCENE SECTION */
+      
+        //Movie Scene Functionality
+
+        //Movie Scene Layout
+
+        /* END OF MOVIE SCENE SECTION */
+
+
+        /* TEXT SCENE SECTION */
+
+        //Text Scene Functionality--used for add and edit
+
+        //Text Scene Layout
+
+        /* END OF TEXT SCENE SECTION */
+
+
+        window.setTitle("Movie Review System");
+        window.setScene(registerScene);
+        window.show();
     }
 }
--- Java FX Tutorial/JavafxSample.java
+++ Java FX Tutorial/JavafxSample.java
@@ -1,35 +1,33 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class JavafxSample extends Application implements EventHandler<ActionEvent> {
+public class JavafxSample extends Application {
+    Stage window;
     Button button;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start (Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Sample Application");
-        button = new Button();
-        button.setText("Click me");
-        button.setOnAction(this);
+        window = primaryStage;
+        window.setTitle("Sample Application");
+
+        button = new Button("Click me");
+        button.setOnAction(e -> {
+            boolean result = ConfirmBox.display("Title of Window", "Are you sure you want to send naked pics?");
+            System.out.println(result);
+        });
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
 
         Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    @Override
-    public void handle (ActionEvent event) {
-        if (event.getSource() == button)
-            System.out.println("I love it when you touch me there");
+        window.setScene(scene);
+        window.show();
     }
 }
